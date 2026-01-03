3e6217d398131ae8b1f983ad98853c37fe3d81d7
Jochem Steenbakkers <jochem.steenbakkers@gmail.com>
2026-01-03 16:03:14 +0100

    Use docs folder for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '/voorraad-beheer/',
+    build: {
+        outDir: 'docs'
+    },
     plugins: [react()],
 });
